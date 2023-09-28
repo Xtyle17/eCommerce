2b6cf03388b2ce3929b54af443c15fe5529c92fa
kyle A <arana.jeffersonkyle@gmail.com>
2023-09-28 22:49:34 +0800

    changed more designs and removed unused parameters

--- src/Components/Body/ProductsList.jsx
+++ src/Components/Body/ProductsList.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useReducer, useContext, useEffect } from "react";
+import React, { useState,  useContext, useEffect } from "react";
 import Footer from "../Footer/Footer";
 import { motion } from "framer-motion";
 import "../../Css/products.css";
 import { Link } from "react-router-dom";
 import AddToCart from "./AddToCart";
 import { CartContext } from "../Provider/cartProvider";
-import { reducer, initialState, ACTIONS } from "../reducer";
+
 
 const ProductsList = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  
   const [currentPage, setCurrentPage] = useState(1);
   const productsPerPage = 5;
   const { searchResult, productType, setProductType } = useContext(CartContext);
@@ -32,17 +32,9 @@ const ProductsList = () => {
   };
 
   return (
-    <div className="box flex flex-col justify-center ">
-      <motion.div
-        className="Body justify-center "
-        initial={{ opacity: 0, width: 0 }}
-        animate={{ opacity: 1, width: "100%" }}
-        exit={{
-          opacity: 0,
-          x: window.innerWidth,
-          transition: { duration: 0 },
-        }}>
-        <form className=" flex flex-col gap-y-0 absolute left-5 top-36  md:left-5 md:top-15 lg:left-5 lg:top-20 ">
+    <>
+    <form className="border-2 w-[130px] p-2 pl-5  m-2 ">
+      <p>Types:</p>
           <label className="flex ">
             <input
               type="radio"
@@ -56,6 +48,7 @@ const ProductsList = () => {
           </label>
 
           <label className="flex">
+            
             <input
               type="radio"
               name="productType"
@@ -77,13 +70,24 @@ const ProductsList = () => {
             wear
           </label>
         </form>
-        <div className="w-48 md:w-3/5 lg:w-5/5 flex flex-col content-center justify-center items-center">
+    <div className="box  flex flex-col justify-center flex-grow flex-shrink ">
+      <motion.div
+        className="Body justify-center flex-wrap "
+        initial={{ opacity: 0, width: 0 }}
+        animate={{ opacity: 1, width: "100%" }}
+        exit={{
+          opacity: 0,
+          x: window.innerWidth,
+          transition: { duration: 0 },
+        }}>
+        
+        <div className="grow-1 w-48 md:w-4/6 h-full lg:w-5/5 flex flex-col content-center justify-center items-center gap-y-5">
           <h2>PRODUCTS</h2>
 
           <motion.div
             className="products w-5/5 flex gap-x-10 "
             initial={{ opacity: 0, width: 0 }}
-            animate={{ opacity: 1, width: "80%" }}
+            animate={{ opacity: 1, width: "100%" }}
             exit={{
               opacity: 0,
               x: window.innerWidth,
@@ -127,10 +131,11 @@ const ProductsList = () => {
           </div>
         </div>
       </motion.div>
-      <div className="absolute left-2/4 bottom-0">
+      <div className="foot">
         <Footer />
       </div>
     </div>
+    </>
   );
 };
 
