600f738220beb0c1a41a146df816d723e37aa558
kyle arana <xtylearana@gmail.com>
2023-10-03 16:52:41 +0800

    added style for the registration

--- src/Components/Navigation/Register.jsx
+++ src/Components/Navigation/Register.jsx
@@ -1,4 +1,6 @@
 import { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../../Css/register.css";
 //mostly should be done in backend
 // const USER_REGEX = /^[a-zA-Z0-9]{3,20}+([._]?[a-zA-Z0-9]+)*$/;
 // const PASS_REGEX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
@@ -34,7 +36,43 @@ const Register = () => {
   //     setValidMatch(match);
   //   }, [pass, matchPass]);
 
-  return <div>Registration</div>;
+  return (
+    <div className="flex flex-col  justify-center items-center min-h-screen bg-amber-300 ">
+      <div
+        className="reg bg-red-500 p-3  md:p-8 flex flex-col gap-y-4 rounded-md w-[220px] sm:w-[500px] md:w-[500px]
+      lg:w-[500px] ">
+        <h1>Registration</h1>
+
+        <form action="" className="flex flex-col  gap-y-2">
+          <input
+            type="email"
+            required
+            placeholder="Ex. user@gmail.com"
+            className="p-2 bg-white"
+          />
+          <input
+            type="password"
+            required
+            placeholder="*******"
+            className="p-2 bg-white"
+          />
+          <input
+            type="user"
+            required
+            placeholder="name"
+            className="p-2 bg-white"
+          />
+          <button className="bg-gray-600 ">Submit</button>
+        </form>
+        <p className="text-sm">
+          Already have an account?{" "}
+          <Link to={"/login"} className="ml-2 text-sm">
+            click here
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
